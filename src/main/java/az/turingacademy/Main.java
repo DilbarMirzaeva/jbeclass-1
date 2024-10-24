cb9f56384aed6a2fbe8da5d5aaba53e18255140d
DilbarMirzaeva <dlbarmirza@gmail.com>
2024-10-24 09:34:40 -0700

    hello world print

--- src/main/java/az/turingacademy/Main.java
+++ src/main/java/az/turingacademy/Main.java
@@ -3,6 +3,9 @@ package az.turingacademy;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.print("Hello,World!\n");
+        System.out.println("Hello,World");
+        System.out.printf("Hello,%s"," World ");
+        System.out.printf("Hello,World: %f",9.63725);
     }
 }
