459591b132e3f8f480596faf5e949369f1c69970
DilbarMirzaeva <dlbarmirza@gmail.com>
2024-10-26 06:53:32 -0700

    2eded toplanmasi

---
+++ src/main/java/class2.java
@@ -0,0 +1,17 @@
+public class class2 {
+    public static void main(String[] args) {
+        int number1=15;
+        int number2=7;
+        int cem=number1+number2;
+        int ferq=number1-number2;
+        int bolme=number1/number2;
+        int hasil=number1*number2;
+        int qaliq=number1%number2;
+
+        System.out.println(cem);
+        System.out.println(ferq);
+        System.out.println(bolme);
+        System.out.println(hasil);
+        System.out.println(qaliq);
+    }
+}
