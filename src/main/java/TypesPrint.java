558a7057b976f6802a347cb85ab48ae2b53e68e1
DilbarMirzaeva <dlbarmirza@gmail.com>
2024-10-26 02:40:26 -0700

    8types and 3print methods

---
+++ src/main/java/TypesPrint.java
@@ -0,0 +1,50 @@
+public class TypesPrint {
+    public static void main(String[] args) {
+        //8 primitive data tipinin 3print method ile hazirlanmasi
+        byte byte1=100;
+        System.out.println(byte1);
+        System.out.printf("byte tipli  %d-dir\n",byte1);
+        System.out.print(byte1 +"\n");
+
+
+        short short1=32000;
+        System.out.println(short1);
+        System.out.printf("short tipli  %d-dir\n",short1);
+        System.out.print(short1 +"\n");
+
+        int int1=640000;
+        System.out.println(int1);
+        System.out.printf("int tipli  %d-dir\n",int1);
+        System.out.print(int1 +"\n");
+
+        long long1=1009800000;
+        System.out.println(long1);
+        System.out.printf("long tipli %d-dir\n",long1);
+        System.out.print(long1+"\n");
+
+        float float1=5.5f;  /* => float float1=(float)5.5 <=belede yazmaq olar*/
+        System.out.println(float1);
+        System.out.printf("float tipli %f-dir\n",float1);
+        System.out.print(float1+"\n");
+
+        double double1=6477.5;
+        System.out.println(double1);
+        System.out.printf("double tipli %f-dir\n",double1);
+        System.out.print(double1+"\n");
+
+        char char1='D';
+        System.out.println(char1);
+        System.out.printf("char tipli %s-dir\n",char1);
+        System.out.print(char1+"\n");
+
+        boolean bool1=true;
+        System.out.println(bool1);
+        System.out.printf("boolean tipli %s-dir\n",bool1);
+        System.out.print(bool1+"\n");
+
+
+
+    }
+
+
+}
